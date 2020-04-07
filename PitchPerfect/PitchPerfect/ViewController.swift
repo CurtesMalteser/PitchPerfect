513ea1c231e5bb1d4c3538fe3fd9824a940236a1
Antonio <antonio@igenius.ai>
2020-04-07 21:21:00 +0200

    Created xcode project

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Antonio on 07.04.20.
+//  Copyright © 2020 CurtesMalteser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
